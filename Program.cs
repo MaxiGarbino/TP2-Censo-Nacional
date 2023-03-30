1a8fbf8c4c89a5ae3c855b6517af69f473771015
MAXIMILIANO GARBARINO <47433817@ort>
2023-03-30 15:05:24 -0300

    Menu puesto al main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+
+Dictionary<int,Persona> personas = new Dictionary<int,Persona>();
+int menu;
+menu = Funciones.IngresarEnteroEnRango("Ingrese la opcion: 1.Cargar nueva persona   2. Obtener Estadisticas del Censo   3.Buscar Persona  4.Modificar Mail de una Persona   5.Salir", 0,6);
+do{
+    switch (menu){
+    case 1:
+        
+        break;
+    case 2:
+        
+        break;
+    case 3:
+        
+        break;
+    case 4:
+        
+        break;
+    case 5:
+        
+        break;
+    }
+}while(menu != 5);
