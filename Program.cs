ab43053faa0fd2f7f32bc093df80f88e0ffa84d1
MATÍAS ABOUDARA <47701750@ort>
2023-03-30 15:35:32 -0300

    CargarPersona terminado y ModificarMail

--- Program.cs
+++ Program.cs
@@ -19,16 +19,34 @@ do{
     case 4:
         
         break;
-    case 5:
-        
-        break;
     }
 }while(menu != 5);
 
 void CargarPersona(){
-    int DNI = Funciones.IngresarEntero("Ingresar DNI");
+    int DNI;
+    bool validar;
+    do{
+    DNI = Funciones.IngresarEntero("Ingresar DNI");
+    validar = !personas.ContainsKey(DNI);
+    } while (!validar);
     string apellido = Funciones.IngresarTexto("Ingresar apellido");
     string nombre = Funciones.IngresarTexto("Ingresar nombre");
     DateTime fechaNacimiento = Funciones.IngresarFecha("Ingresar fecha de nacimiento aaaa/mm/dd");
     string email = Funciones.IngresarTexto("Ingresar Email");
+    Persona persona1 = new Persona(DNI, apellido, nombre, fechaNacimiento, email);
+    personas.Add(DNI, persona1);
+    Console.WriteLine($"Se ha creado la persona {nombre} {apellido} y se ha agregado a la lista.");
+}
+
+void ModificarMail(){
+    int DNI;
+    bool validar;
+    DNI = Funciones.IngresarEntero("Ingresar DNI");
+    validar = !personas.ContainsKey(DNI);
+    while(!validar){
+        Console.WriteLine("No se encuentra el DNI");
+        DNI = Funciones.IngresarEntero("Ingresar DNI");
+        validar = !personas.ContainsKey(DNI);
+    }
+    personas[DNI].Email = Funciones.IngresarTexto("Ingresar nuevo mail");
 }
