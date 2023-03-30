dcf9efa8002a4f25f833096817de699ee086f855
MATÍAS ABOUDARA <47701750@ort>
2023-03-30 15:07:03 -0300

    Agrego CargarPersona

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+
+Dictionary<int, Persona> personas = new Dictionary<int, Persona>();
+
+void CargarPersona(){
+    int DNI = Funciones.IngresarEntero("Ingresar DNI");
+    string apellido = Funciones.IngresarTexto("Ingresar apellido");
+    string nombre = Funciones.IngresarTexto("Ingresar nombre");
+    DateTime fechaNacimiento = Funciones.IngresarFecha("Ingresar fecha de nacimiento aaaa/mm/dd");
+    string email = Funciones.IngresarTexto("Ingresar Email");
+}
